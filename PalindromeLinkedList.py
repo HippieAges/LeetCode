2d8ee571ff9c0dbb214968b350ceb017e37001fa
HippieAges <OmelchenkoOleksiy@gmail.com>
2021-07-17 18:03:50 -0700

    cleaned up palindrome linked list

--- PalindromeLinkedList.py
+++ PalindromeLinkedList.py
@@ -6,17 +6,16 @@ class Solution:
     def isPalindrome(self, head: ListNode) -> bool:
         if head == None:
             return False
+        if head.next == None:
+            return True
         
-        def iterateBoth(head: ListNode, tail: ListNode) -> Union[bool, ListNode, ListNode]:
+        def iterateBoth(head: ListNode, tail: ListNode) -> Union[None, ListNode]:
             if tail.next == None:
-                if head.val == tail.val:
-                    return head.next
-                else:
-                    return False
+                return head.next if head.val == tail.val else None
             
             headNode = iterateBoth(head, tail.next)
-            if headNode == False or headNode.val != tail.val:
-                return False
-            return headNode.next
-            
-        return True if iterateBoth(head, head) != False else False
+            if not headNode or headNode.val != tail.val:
+                return None
+            return ListNode() if headNode.next == None else headNode.next 
+        
+        return iterateBoth(head, head) != None
