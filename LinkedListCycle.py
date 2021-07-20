a059540a86aa80e495841098a65ec81d1dee2ec1
HippieAges <OmelchenkoOleksiy@gmail.com>
2021-07-20 10:29:13 -0700

    using Floyd's Cycle Detection algorithm for linked list cycle

--- LinkedListCycle.py
+++ LinkedListCycle.py
@@ -8,7 +8,8 @@ class Solution:
     def hasCycle(self, head: ListNode) -> bool:
         # have two pointers, one moves twice as fast as the previous one
         # if not fast.next and not fast then we don't have a cycle
-        
+        # this utilizes Floyd's Cycle Detection algorithm
+
         fast, slow = head, head
         
         while fast and fast.next:
