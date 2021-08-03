7841cd3014d2718622632e80a61ee9184d340648
HippieAges <OmelchenkoOleksiy@gmail.com>
2021-08-03 10:32:29 -0700

    optimized solution for group anagrams

--- GroupAnagrams.py
+++ GroupAnagrams.py
@@ -1,23 +1,20 @@
 from collections import defaultdict
+from itertools import reduce
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         # go through the list and sort each string 
         # and store the sorted string as a key and the index
         # + unsorted string as a tuple pair
-        # runtime is O(n^2log(n) + n^2)
-        # space complexity is O(n + n)
+        # runtime is O(nklog(k))
+        # space complexity is O(nk)
         
         if len(strs) == 1:
             return [[strs[0]]]
         
         sorted_strs = defaultdict(list)
-        anagrams = []
         
-        for idx, curr_str in enumerate(strs):
+        for curr_str in strs:
             sorted_str = reduce(lambda x, y: x + y, sorted(curr_str)) if curr_str else ""
-            sorted_strs[sorted_str].append( (curr_str, idx) )
-            
-        for key in sorted_strs:
-            anagrams.append([curr_str for curr_str, _ in sorted_strs[key] ])
+            sorted_strs[sorted_str].append(curr_str)
         
-        return anagrams
+        return sorted_strs.values()
